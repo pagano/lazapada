b789173e69b5e00f128511ebb5cafc2e27e91bee
Jorge Pagano <jorgepagano@gmail.com>
2023-05-26 10:33:47 -0300

    /

--- web/index.js
+++ web/index.js
@@ -4,62 +4,32 @@ incs.push({ id: 'bootstrapcss', src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0
 incs.push({ id: 'jquery', src: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js' });
 incs.push({ id: 'indexcss', src: gitCdn(Object.assign(gitParams, { path: 'web/index.css' })) });
 
+var $d, $b;
+
 include(incs).then(async () => {
+  var $d = $(document);
   var $b = $(document.body);
   $b.attr('cz-shortcut-listen', 'true');
   $b.css('background', '#EEEAE7');
   var res = await fetch(gitCdn(Object.assign(gitParams, { path: 'web/index.htm' })));
-  $b.append(await res.text());  
+  $b.append(await res.text());
+
+  $d.ready(getStats);
+  setInterval(getStats, 10000);
 });
 
-/*
-</link>
-var lastPlaying;
+async function getStats() {
+  var res = await fetch('http://lazapada.net:8000/statistics?json=1');
+  var jsn = await res.json();
+  debugger;
 
-$(document).ready(getStats);
-setInterval(getStats, 10000);
-
-function getStats() {
-    $.ajax({
-        url: "http://lazapada.net:8000/statistics?json=1",
-        dataType: 'JSONP',
-        type: "GET",
-        success: function (data) {
-            $('#listeners_1').html(data.streams[0].currentlisteners);
-            $('#playing_1').html(data.streams[0].songtitle);
-            
-            /
-            // Esta parte actualizaba el player de la radio, que ya no esta
-            $('#listeners_2').html(data.streams[1].currentlisteners);
-
-            var playing = data.streams[1].songtitle;
-            var next = data.streams[1].nexttitle;
-            if (!lastPlaying) {
-                // 1er seteo
-                chPlaying2(playing, next);
-                lastPlaying = playing;
-            } else {
-                if (playing != lastPlaying) {
-                    var a2 = $('#audio_2')[0];
-                    if (a2.buffered.length > 0) {
-                        // Esta en play, cambia cdo termine lo que tengo en buffer
-                        var buff = a2.buffered.end(0) - a2.buffered.start(0);
-                        console.log('buff: ' + buff);
-                        setTimeout(chPlaying2, buff * 1000, playing, next);
-                    } else {
-                        // No esta en play, cambia ya
-                        chPlaying2(playing, next);
-                    }
-                    lastPlaying = playing;
-                }
-            }
-            /
-        }
-    });
+  $.ajax({
+    url: 'http://lazapada.net:8000/statistics?json=1',
+    dataType: 'JSONP',
+    type: 'GET',
+    success: function (data) {
+        $('#listeners_1').html(data.streams[0].currentlisteners);
+        $('#playing_1').html(data.streams[0].songtitle);
+    }
+  });
 }
-
-function chPlaying2(curr, next) {
-    $('#playing_2').html(curr);
-    $('#next_2').html(next);
-}
-*/
